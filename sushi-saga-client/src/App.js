6ba675c7e0db8e332595e64397d55c1e94c76a8f
natashabuck <natashabuck1@gmail.com>
2018-12-17 15:17:48 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,15 +6,76 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  
+  constructor() {
+    super()
+      this.handleShowMore = this.handleShowMore.bind(this)
+      this.handleEat = this.handleEat.bind(this)
+      this.state = {
+        sushis: [],
+        showSushiStart: 0,
+        showSushiEnd: 4,
+        sushiEaten: [],
+        remainingMoney: 50
+      }
+  }
+
+  handleShowMore(){
+    const startShow = this.state.showSushiStart + 4;
+    const endShow = this.state.showSushiEnd + 4;
+    
+    this.setState({
+      showSushiStart: startShow,
+      showSushiEnd: endShow
+    })
+  }
+
+  handleEat = (sushi)=> {
+    let plateCount  = this.state.sushiEaten.concat(sushi)
+    let moneyCount = this.state.remainingMoney - sushi.price
+    
+    if (moneyCount > 0) {
+     (sushi.eaten = true).persist
+    } else {
+      alert('You need more money before you can eat anymore.')
+      moneyCount = 0
+    }
+      
+      this.setState({
+        sushiEaten: plateCount,
+        remainingMoney: moneyCount
+      })
+  }
 
   render() {
+    const sushis = this.state.sushis.slice(this.state.showSushiStart, 
+    this.state.showSushiEnd);
+
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer 
+          sushis={sushis} 
+          handleShowMore={this.handleShowMore}
+          handleEat={this.handleEat} 
+        />
+        <Table 
+          sushiEaten={this.state.sushiEaten}
+          remainingMoney={this.state.remainingMoney}
+        />
       </div>
     );
   }
+
+  componentDidMount(){
+    fetch(API)
+      .then(resp => resp.json())
+      .then(data => 
+        {this.setState({
+          sushis: data
+        })}
+      )
+  }
+
 }
 
 export default App;
